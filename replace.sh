497daa559f4734a14ed8b69de04cc110cf02b313
milenmit <43420986+milenmit@users.noreply.github.com>
2024-09-20 15:44:57 +0300

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -5,9 +5,14 @@ if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
-# Check if DOMAIN is set
+# Check if DOMAIN and API_KEY are set
 if [ -z "$DOMAIN" ]; then
     echo "DOMAIN is not set in the .env file. Exiting."
+    exit 1
+fi
+
+if [ -z "$API_KEY" ]; then
+    echo "API_KEY is not set in the .env file. Exiting."
     exit 1
 fi
 
@@ -19,10 +24,11 @@ FILES=(
     "./flask_app/templates/index.html"
 )
 
-# Replace {{domain}} with the actual domain in each file
+# Replace {{domain}} and {{API_KEY}} with the actual values in each file
 for file in "${FILES[@]}"; do
     if [ -f "$file" ]; then
         sed -i "s/{{domain}}/$DOMAIN/g" "$file"
+        sed -i "s/{{API_KEY}}/$API_KEY/g" "$file"
         echo "Updated $file"
     else
         echo "File $file does not exist. Skipping."
